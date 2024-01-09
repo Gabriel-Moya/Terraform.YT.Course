6aadfb791fb9cdc2c4ae71a2d785968300056cd5
Gabriel Moya <gabrielmoya123@gmail.com>
2024-01-09 17:35:25 -0300

    Add remote state configuration and resources

---
+++ udemy-section04-intermediary-terraform/01-remote-state/01-using-remote-state/variables.tf
@@ -0,0 +1,11 @@
+variable "ami" {
+  type        = string
+  description = ""
+  default     = "ami-0c7217cdde317cfec"
+}
+
+variable "instance_type" {
+  type        = string
+  description = ""
+  default     = "t2.micro"
+}
