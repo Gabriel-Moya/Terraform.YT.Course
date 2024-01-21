94fb32667193d16c826508a44f5c99c2a1e2678e
Gabriel Moya <gabrielmoya123@gmail.com>
2024-01-21 17:07:26 -0300

    Add public and private subnets to VPC

--- udemy-section07-auto-scaling/terraform/vpc.tf
+++ udemy-section07-auto-scaling/terraform/vpc.tf
@@ -7,3 +7,31 @@ resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
   tags   = merge(local.common_tags, { Name = "Terraform Internet Gateway" })
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "192.168.0.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags              = merge(local.common_tags, { Name = "Terraform Public Subnet A" })
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "192.168.1.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags              = merge(local.common_tags, { Name = "Terraform Public Subnet B" })
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "192.168.2.0/24"
+  availability_zone = "${var.aws_region}a"
+  tags              = merge(local.common_tags, { Name = "Terraform Private Subnet A" })
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = "192.168.3.0/24"
+  availability_zone = "${var.aws_region}b"
+  tags              = merge(local.common_tags, { Name = "Terraform Private Subnet B" })
+}
