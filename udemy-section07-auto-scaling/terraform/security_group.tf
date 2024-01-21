b7929aa88f438c42bed2e255fc6cc62919deca32
Gabriel Moya <gabrielmoya123@gmail.com>
2024-01-21 19:09:00 -0300

    Add security group resources

---
+++ udemy-section07-auto-scaling/terraform/security_group.tf
@@ -0,0 +1,71 @@
+resource "aws_security_group" "web" {
+  name        = "Web"
+  description = "Allow public inbound traffic"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_subnet.this["pvt_a"].cidr_block]
+  }
+
+  tags = merge(local.common_tags, { Name = "Web server" })
+}
+
+resource "aws_security_group" "db" {
+  name        = "Database"
+  description = "Allow incomming database connections"
+  vpc_id      = aws_vpc.this.id
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = [aws_vpc.this.cidr_block]
+  }
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, { Name = "Database MySQL" })
+}
